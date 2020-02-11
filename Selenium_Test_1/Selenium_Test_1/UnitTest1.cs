f07331cacc7b615c41ecfdfe40b4f2284672648f
Leonid Bereznytskyi <LBereznytskyi@verismic.com>
2020-02-11 19:44:09 +0200

    Selenium_New

---
+++ Selenium_Test_1/Selenium_Test_1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Selenium_Test_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = 1;
+        }
+    }
+}
